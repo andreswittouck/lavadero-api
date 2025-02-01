ddb7eced5f507771914fbac0b75b9f1fbd745bf8
andreswittouck <andreswittouck@gmail.com>
2025-01-31 21:33:47 -0300

    fix dockerfile

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -2,11 +2,10 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'lavadero',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true, // Usar solo en desarrollo
 });
